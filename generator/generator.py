58d7f881a1e53fadb153a50f558b2f5f3ae22de7
Evgengrmit <evgengrmit@gmail.com>
2020-11-26 17:46:05 +0300

    All version 1 of generator

--- generator/generator.py
+++ generator/generator.py
@@ -21,69 +21,82 @@ from scapy.all import *
 import collections as coll
 
 Source_obj = coll.namedtuple('Source_obj',
-                             ['source_ip', 'source_port', 'source_port', 'protocol', 'packets_count'])
-Destination_obj = coll.namedtuple('Destination_obj',['dest_ip',])
+                             ['source_ip', 'source_port', 'protocol', 'packets_count'])
+Destination_obj = coll.namedtuple('Destination_obj', ['dest_ip', 'dest_port'])
+
 
 class Generator:
 
-    def __init__(self, path="", isSrc=False, isDst=False) -> None:
+    def __init__(self, path="", is_src=False, is_dst=False) -> None:
         self.path_ = path
         self.ddos_data_ = []
-        self.source_ =
+        self.source_ = None
         self.source_data = Source_obj
-        self.destination_ = Destination_obj
+        self.destination_data = Destination_obj
         if path != "":
             file = open(self.path_, "r")
             csv_reader = csv.DictReader(file, delimiter=",")
             for data in csv_reader:
                 self.ddos_data_.append(data)
             file.close()
-        if isSrc:
-            source = rd.choice(self.ddos_data_)
-            self.source_data.source_ip = source['Src IP']
-            self.source_data.source_port = int(rd.choice(source['Src Port'].split(' ')))
-            self.source_data.protocol = int(rd.choice(source['Protocol'].split(' ')))
-            self.source_data.packets_count = int(float(source['Tot Fwd Pkts']))
-        if isDst:
+        if is_src:
+            self.source_ = rd.choice(self.ddos_data_)
+            self.source_data.source_ip = self.source_['Src IP']
+            self.source_data.source_port = int(rd.choice(self.source_['Src Port'].split(' ')))
+            self.source_data.protocol = int(rd.choice(self.source_['Protocol'].split(' ')))
+            self.source_data.packets_count = int(float(self.source_['Tot Fwd Pkts']))
+        if is_dst:
+            self.destination_data.dest_ip = rd.choice(self.source_['Dst IP'].split(' '))
+            self.destination_data.dest_port = int(rd.choice(self.source_['Dst Port'].split(' ')))
 
     # Шаг 1.
     def get_data(self, path=""):
         if path != "":
             self.path_ = path
-            if path == "":
-                raise IOError("Empty path")
-            file = open(self.path_, "r")
-            csv_reader = csv.DictReader(file, delimiter=",")
-            for data in csv_reader:
-                self.ddos_data_.append(data)
-            file.close()
+        else:
+            raise IOError("Empty path")
+        file = open(self.path_, "r")
+        csv_reader = csv.DictReader(file, delimiter=",")
+        for data in csv_reader:
+            self.ddos_data_.append(data)
+        file.close()
         return self.ddos_data_
 
     # Шаг 2.
     def get_random_source(self):
-        source = rd.choice(self.ddos_data_)
-        self.source_.source_ip = source['Src IP']
-        self.source_.source_port = int(rd.choice(source['Src Port'].split(' ')))
-        self.source_.protocol = int(rd.choice(source['Protocol'].split(' ')))
-        self.source_.packets_count = int(float(source['Tot Fwd Pkts']))
+        self.source_ = rd.choice(self.ddos_data_)
+        self.source_data.source_ip = self.source_['Src IP']
+        self.source_data.source_port = int(rd.choice(self.source_['Src Port'].split(' ')))
+        self.source_data.protocol = int(rd.choice(self.source_['Protocol'].split(' ')))
+        self.source_data.packets_count = int(float(self.source_['Tot Fwd Pkts']))
         return self.source_
 
     # Шаг 3.
     def get_random_destination(self):
-        dest_ip = rd.choice(source['Dst IP'].split(' '))
-        dest_port = int(rd.choice(source['Dst Port'].split(' ')))
+        self.destination_data.dest_ip = '.'.join('%s' % random.randint(0, 255) for i in range(4))
+        #self.destination_data.dest_ip = rd.choice(self.source_['Dst IP'].split(' '))
+        self.destination_data.dest_port = int(rd.choice(self.source_['Dst Port'].split(' ')))
+        return self.destination_data
 
-# Шаг 4.
+    # Шаг 4.
+    def create_packet(self):
+        packet_ = IP(src=self.source_data.source_ip, dst=self.destination_data.dest_ip) / TCP(
+            sport=self.source_data.source_port, dport=self.destination_data.dest_port) / "TEST"
+        return packet_
 
-packets = IP(src=source_ip, dst=dest_ip) / TCP(sport=source_port, dport=dest_port) / "TEST"
+    # Шаг 5.
+    def create_pcap(self, pack):
+        pcap_filename = f"{self.source_data.source_ip}_{datetime.today().strftime('%d.%m.%Y')}.pcap"
+        wrpcap(pcap_filename, pack)
+        return pcap_filename
 
-# Шаг 5.
-
-pcap_filename = f"{source_ip}_{datetime.today().strftime('%d.%m.%Y')}.pcap"
-wrpcap(pcap_filename, packets)
 
 # Шаг 6.
-
-readed_packers = rdpcap(pcap_filename)
-
-print(readed_packers.show())
+if __name__ == '__main__':
+    gen = Generator()
+    gen.get_data("ddos_src.csv")
+    gen.get_random_source()
+    gen.get_random_destination()
+    my_packet = gen.create_packet()
+    readed_packets = rdpcap(gen.create_pcap(my_packet))
+    print(readed_packets.show())
